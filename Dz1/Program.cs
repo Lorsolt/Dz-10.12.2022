b72fa73fdb8c456db2134993fbdda5f82121f86e
Lorsolt <mr.lorsolt@mail.ru>
2022-12-10 17:37:57 +0300

    Решено!

---
+++ Dz1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int a = InputInt("Введите  число: ");
+int b = 1;
+int Numbers(int a, int b)
+{
+    if (a == b)
+        return a;
+    else
+        Console.Write($"{Numbers(a, b + 1)}, ");
+    return b;
+}
+int InputInt(string numb)
+{
+    Console.Write(numb);
+    return int.Parse(Console.ReadLine());
+}
+Console.WriteLine(Numbers(a, b));
+
+
+
---
+++ Dz2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+int a = InputInt("Введите первое число: ");
+int b = InputInt("Введите второе число: ");
+int InputInt(string numb)
+{
+    Console.Write(numb);
+    return int.Parse(Console.ReadLine());
+}
+int NumberSum(int a, int b)
+{
+    if (a == b)
+        return b;
+    return b + NumberSum(a, b - 1);
+}
+Console.WriteLine($"Сумма элементов от {a} до {b} = {NumberSum(a, b)}");
